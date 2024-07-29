9b247653ff8031aa169f4a6dcd204f0d33878e56
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2024-07-29 00:46:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Particles
+===============
+
+Rendering algorithms implemented in WebGPU.
+
+- [Triangle](https://tsherif.github.io/webgpu-examples/triangle.html): Creating vertex buffers and drawing a triangle.
+- [Cube](https://tsherif.github.io/webgpu-examples/cube.html): Creating a texture from an ImageBitmap, setting up vertex and uniform buffers, and animating a 3D cube.
+- [Particles](https://tsherif.github.io/webgpu-examples/particles.html): Simulating gravity on instanced particles using a compute shader.
+- [Multiple Canvases](https://tsherif.github.io/webgpu-examples/multi-canvas.html): Rendering to multiple canvases with a single device instance.
+- [Picking](https://tsherif.github.io/webgpu-examples/pick.html): Interact with rendered objects using color picking.
+- [Deferred Rendering](https://tsherif.github.io/webgpu-examples/deferred.html): Rendering mesh data to a multisampled gBuffer then lighting in a separate pass.
+
+All examples are implemented in a single HTML file with minimal use of functions, modules, classes or other abstractions. The goal is to allow the reader to easily see, in sequential order, all WebGPU calls that are made. They can be run locally by serving them from a local HTTP server, e.g. `python -m http.server`.
+
---
+++ utils/Timer.js
@@ -0,0 +1,134 @@
+const DEFAULT_SAMPLE_COUNT = 50;
+
+export class Timer {
+    static PASS_START = 1;
+    static PASS_END = 2;
+
+    constructor(device, sampleCount = DEFAULT_SAMPLE_COUNT) {
+        this.device = device;
+        this.sampleCount = sampleCount;
+        this.hasGPUTimer = device.features.has("timestamp-query");
+        this.cpuTimers = {};
+        this.cpuTimes = {};
+        this.gpuTimers = {};
+        this.gpuTimes = {}
+    }
+
+    gpuPassDescriptor(timerName, flags = Timer.PASS_START | Timer.PASS_END) {
+        if (!this.hasGPUTimer) {
+            return undefined;
+        }
+
+        if (!this.gpuTimers[timerName]) {
+            const querySet = this.device.createQuerySet({
+                type: "timestamp",
+                count: 2
+            });
+    
+            const resolveBuffer = this.device.createBuffer({
+                size: querySet.count * 8,
+                usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC
+            });
+    
+            const resultBuffer = this.device.createBuffer({
+                size: resolveBuffer.size,
+                usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+            });   
+
+            this.gpuTimers[timerName] = {
+                querySet,
+                resolveBuffer,
+                resultBuffer,
+                time: 0,
+                timeCount: 0,
+            }
+
+            this.gpuTimes[timerName] = 0;
+        }
+
+        const timer = this.gpuTimers[timerName];
+
+        const descriptor = {
+            querySet: timer.querySet
+        };
+
+        if (flags & Timer.PASS_START) {
+            descriptor.beginningOfPassWriteIndex = 0;
+        }
+
+        if (flags & Timer.PASS_END) {
+            descriptor.endOfPassWriteIndex = 1;
+        }
+
+        return descriptor;
+    }
+
+    gpuBeforeSubmit(commandEncoder, timerNames = Object.keys(this.gpuTimers)) {
+        timerNames.forEach(timerName => {
+            const timer = this.gpuTimers[timerName];
+
+            if (timer && timer.resultBuffer.mapState === "unmapped") {
+                const { querySet, resolveBuffer, resultBuffer } = timer;
+
+                commandEncoder.resolveQuerySet(querySet, 0, querySet.count, resolveBuffer, 0);
+                commandEncoder.copyBufferToBuffer(resolveBuffer, 0, resultBuffer, 0, resultBuffer.size);
+            }
+        });
+    }
+
+    gpuAfterSubmit(timerNames = Object.keys(this.gpuTimers)) {
+        timerNames.forEach(timerName => {
+            const timer = this.gpuTimers[timerName];
+
+            if (timer && timer.resultBuffer.mapState === "unmapped") {
+                const { resultBuffer } = timer;
+                resultBuffer.mapAsync(GPUMapMode.READ).then(() => {
+                    const [start, end] = new BigInt64Array(resultBuffer.getMappedRange());
+                    resultBuffer.unmap();
+    
+                    const time = Number(end - start);
+    
+                    if (time >= 0) {
+                        timer.time += time;
+                        ++timer.timeCount;
+                    } else {
+                        timer.time = 0;
+                        timer.timeCount = 0;
+                    }
+    
+                    if (timer.timeCount === this.sampleCount) {
+                        this.gpuTimes[timerName] = timer.time / this.sampleCount / 1000000;
+                        timer.time = 0;
+                        timer.timeCount = 0;
+                    }
+                });
+            }
+        });
+    }
+
+    cpuTimeStart(timerName) {
+        this.cpuTimers[timerName] ??= {
+            startTime: 0,
+            time: 0,
+            timeCount: 0
+        };
+        this.cpuTimes[timerName] ??= 0;
+
+        this.cpuTimers[timerName].startTime = performance.now();
+    }
+
+
+    cpuTimeEnd(timerName) {
+        const timer = this.cpuTimers[timerName];
+
+        timer.time += performance.now() - timer.startTime;
+        ++timer.timeCount;
+
+        if (timer.timeCount === this.sampleCount) {
+            this.cpuTimes[timerName] = timer.time / this.sampleCount;
+            timer.time = 0;
+            timer.timeCount = 0;
+        }
+    }
+
+}
